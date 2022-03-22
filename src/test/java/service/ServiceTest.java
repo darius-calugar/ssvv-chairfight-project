0216543e6dbe3f156cd1d137e49ae4d5e5b4d346
Cata <catab212@gmail.com>
2022-03-22 19:05:52 +0200

    Added all test cases

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -2,24 +2,25 @@ package service;
 
 import domain.Student;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepo;
 import repository.StudentXMLRepo;
 import repository.TemaXMLRepo;
-import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.ValidationException;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 class ServiceTest {
     private Service service;
+
+    Student makeValidStudent() {
+        return new Student("uniqueId", "nume", 1, "mail@mail.mail");
+    }
 
     String makeTempFile(String inputResource, String extension) throws IOException {
         var tempFile = Files.createTempFile(java.util.UUID.randomUUID().toString(), extension);
@@ -39,13 +40,117 @@ class ServiceTest {
         service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
     }
 
+
+
     @Test
     void addStudent_correctGroup_success() {
-        Assertions.assertNull(service.addStudent(new Student("uniqueId", "nume", 20, "mail@mail.mail")));
+        var student = makeValidStudent();
+        student.setGrupa(20);
+        Assertions.assertNull(service.addStudent(student));
     }
 
     @Test
     void addStudent_negativeGroup_ValidationException() {
-        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(new Student("uniqueId", "nume", -20, "mail@mail.mail")));
+        var student = makeValidStudent();
+        student.setGrupa(-1);
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_group0_Success() {
+        var student = makeValidStudent();
+        student.setGrupa(0);
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_group1_Success() {
+        var student = makeValidStudent();
+        student.setGrupa(1);
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_groupSmallerThanMaxInt_Success() {
+        var student = makeValidStudent();
+        student.setGrupa(Integer.MAX_VALUE - 1);
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_groupMaxInt_Success() {
+        var student = makeValidStudent();
+        student.setGrupa(Integer.MAX_VALUE);
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_groupLargerThanMaxInt_ValidationException() {
+        var student = makeValidStudent();
+        student.setGrupa(Integer.MAX_VALUE + 1); // equivalent to -(MAX_VALUE + 1) -> fails at < 0
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_idNonEmpty_Success() {
+        var student = makeValidStudent();
+        student.setID("uniqueId");
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_idNull_ValidationException() {
+        var student = makeValidStudent();
+        student.setID(null);
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_idEmpty_ValidationException() {
+        var student = makeValidStudent();
+        student.setID("");
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_nameNonEmpty_ValidationException() {
+        var student = makeValidStudent();
+        student.setNume("nume");
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_nameNull_ValidationException() {
+        var student = makeValidStudent();
+        student.setNume(null);
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_nameEmpty_ValidationException() {
+        var student = makeValidStudent();
+        student.setNume("");
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_emailNonEmpty_ValidationException() {
+        var student = makeValidStudent();
+        student.setEmail("mail@mail.mail");
+        Assertions.assertNull(service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_emailNull_ValidationException() {
+        var student = makeValidStudent();
+        student.setEmail(null);
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    void addStudent_emailEmpty_ValidationException() {
+        var student = makeValidStudent();
+        student.setEmail("");
+        Assertions.assertThrows(ValidationException.class, () -> service.addStudent(student));
     }
 }
